fe8a4456f19526a89797c0160021e8b62869dff1
Luciano Junior <engluciano.junior@gmail.com>
2021-01-25 01:46:28 -0300

    Routes and Templates

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const handlebars = require('express-handlebars');
 
 const app = express();
 
+//TEMPLATE ENGINE
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+//routes and templates
+app.get('/' , function (req, res) {
+    //res.send('Mensagem de teste') MANDANDO UMA MSG SIMPLES
+    //res.sendFile(__dirname+"/index.html"); MANDANDO UMA ARQUIVO ESPECIFICO
+    res.render('index');
+    /*console.log(req.params.id); PARAMETROS COM ID*/
+});
+
 
 //START SERVER
 
